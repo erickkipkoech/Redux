b98be86d18885817f2a5455aab50404b06602ddc
kipkoech eric <cheruiyotkipkoech612@gmail.com>
2024-07-22 13:05:57 +0000

    redux

---
+++ src/reducer.js
@@ -0,0 +1,53 @@
+import { act } from "react";
+
+const initialState = {
+    todos: [
+      { id: 0, text: 'Learn React', completed: true },
+      { id: 1, text: 'Learn Redux', completed: false, color: 'purple' },
+      { id: 2, text: 'Build something fun!', completed: false, color: 'blue' }
+    ],
+    filters: {
+      status: 'All',
+      colors: []
+    }
+  }
+
+  function nextTodoId(todos){
+    const maxId=todos.reduce((maxId,todo)=>Math.max(todo.id,maxId),-1)
+    return maxId+1;
+  }
+
+  export default function appReducer(state=initialState,action){
+    switch(action.type){
+        case 'todos/todoAdded':{
+                return{
+                    ...state,
+                    todos:[
+                        ...state.todos,
+                        {
+                            id:nextTodoId(state.todos),
+                            text:action.payload,
+                            completed: false
+                        }
+                    ]
+                }
+            }
+        case 'todos/todoToggeled':{
+            return{
+                ...state,
+                todos:state.todos.map(todo=>{
+                    if(todo.id!==action.payload){
+                        return todo;
+                    }
+                    return {
+                        ...todo,
+                    }
+                })
+            }
+        }
+        default: 
+        return state;
+    }
+  }
+
+  
